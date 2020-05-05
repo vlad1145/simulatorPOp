6e6a279c89a127c0bac646b186abb0bbd0e6e732
agataaag <62572893+agataaag@users.noreply.github.com>
2020-05-05 16:11:00 +0200

    Update projekt.h

--- projekt.h
+++ projekt.h
@@ -8,34 +8,67 @@ protected:
     bool zdolnosc_ruchu;
     int energia;
     int min_energia_podzialu;
-
+    int energia_p;
+    int polee;
+    int pozywienie;
+    int trucizna;
+    int zebrane_H20;
+    int zebrane_CO2;
+    int zebrana_glukoza;
+    int zebrany_tlen;
+    int zebrany_etanol;
+    int zebrany_metanol;
+    int organizm;
 public:
-    Mikroby(int energia, int min_energia_podzialu, char organizm);
+    Mikroby(int energia, int energia_p, int min_energia_podzialu);
     ~Mikroby();
-    int get_energia();
-    void zbierz();
-    void przerobi();
+    int get_energia(int polee);
+    void zbierz(int pozywienie, int trucizna);
+    void przerob();
     void podziel(int x);
     void skonaj();
 };
 
-class Samozywne : public Mikroby
+class Samozywne: public Mikroby
 {
+	int zebrane_H2O;
+	int zebrane_CO2;
+	int zatrucie;
+	int maxymalne_zatrucie;
+
+	public:
+		int getH2O();
+		int getCO2();
+		int getZatrucie();
+		int getMaxZatrucie();
+		void odtruj();
+};
+
+class Cudzozywne: public Mikroby
+{
+	int zebrana_glukoza;
+	int zebrany_tlen;
+	int zatrucie;
+	int maxymalne_zatrucie;
+
+	public:
+		int getGlukoza();
+		int getTlen();
+		int getZatrucie();
+		int getMaxZatrucie();
+		void odtruj();
 
 };
 
-class Cudzozywne : public Mikroby
+class Reducenci: public Mikroby
 {
+	int zebrany_etanol;
+	int zebrany_metan;
 
-
+	public:
+		int getEtanol();
+		int getMetan();
 };
-
-class Reducenci : public Mikroby
-{
-
-
-};
-
 
 
 void nieZnika(int x, int y);
